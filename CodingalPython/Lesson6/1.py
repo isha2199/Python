12e1924e33c677ffa682c3071086645ac36205cc
isha.goyal <ishagoyal2199@gmail.com>
2026-09-11 10:29:29 +0530

    lesson 6

---
+++ CodingalPython/Lesson6/1.py
@@ -0,0 +1,10 @@
+weather = input("Enter today's weather (sunny, rainy, cloudy): ")
+
+# Topic 2 - AND operator
+if weather == "sunny" and homework == "yes":
+    print("After school: Head to the park!")
+
+# Topic 3 - OR operator
+if weather == "rainy" or weather == "cloudy":
+    print("Weather tip : Pack your umbrella!")
+
