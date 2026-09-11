743d2f75c9756dc99dc50ea70ca68572fbbcd6de
isha.goyal <ishagoyal2199@gmail.com>
2026-09-11 10:35:21 +0530

    lesson 7 commit

---
+++ CodingalPython/Lesson7/2.py
@@ -0,0 +1,14 @@
+# Six bitwise operators:
+# &   AND
+# |   OR
+# ^   XOR
+# ~   NOT
+# <<  Left Shift
+# >>  Right Shift
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("AND:", a & b)
+print("OR:", a | b)
+print("XOR:", a ^ b)
