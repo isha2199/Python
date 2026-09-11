88be0f979d9223723b2cfd69be615f761df94ab0
isha.goyal <ishagoyal2199@gmail.com>
2026-09-11 10:07:43 +0530

    commit python

--- CodingalPython/Lesson 2/main1.py
+++ CodingalPython/Lesson 2/main1.py
@@ -1,6 +1,3 @@
-import keyword
-print(keyword.kwlist)
-
 print("hello", 5)
 # output: hello 5
 
@@ -10,11 +7,3 @@ print("Hello Python \n")
 
 print("welcome to ", end="*")
 # output: welcome to *   (no new line after)
-
-x = 5
-y = "John"
-codingal = "codingal"
-name = input("enter your name: ")
-# all four are valid identifiers
-
-print(x, y, codingal, name)
