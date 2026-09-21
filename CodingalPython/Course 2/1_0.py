4224705302048987ab400c636ebf9e0ec2b05a1f
isha.goyal <ishagoyal2199@gmail.com>
2026-09-21 14:47:49 +0530

    module 2 commit

---
+++ CodingalPython/Course 2/1_0.py
@@ -0,0 +1,6 @@
+attendance = float(input("Enter your attendance percentage: "))
+
+if attendance >= 75:
+    print("You are eligible to sit for the exam.")
+else:
+    print("You are not eligible to sit for the exam.")
