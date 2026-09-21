a41ffc24a39401cbb5a9845008dec608b6637610
isha.goyal <ishagoyal2199@gmail.com>
2026-09-21 15:05:03 +0530

    module 2 commit

---
+++ CodingalPython/Course 2/6_2.py
@@ -0,0 +1,6 @@
+sides = int(input("Enter the number of sides: "))
+
+if sides >= 3:
+    print("It is a polygon.")
+else:
+    print("It is not a polygon.")
---
+++ CodingalPython/Course 2/6_5.py
@@ -0,0 +1,7 @@
+n = int(input("Enter the size: "))
+
+for i in range(n):
+    if i == 0 or i == n - 1:
+        print("* " * n)
+    else:
+        print("*" + " " * (2 * n - 3) + "*")
